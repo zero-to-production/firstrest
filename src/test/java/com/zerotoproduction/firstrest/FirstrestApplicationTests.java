14007cf98adf16f0430c09a23514d1d514d682e3
paultofunmi <paultofunmi01@gmail.com>
2019-03-28 18:23:54 -0700

    first commit

---
+++ src/test/java/com/zerotoproduction/firstrest/FirstrestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zerotoproduction.firstrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FirstrestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
